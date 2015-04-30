4bfdb89c78bdfc705a136fcf67f6af99c2b280eb
Sebastian Graca | hybris <sebastian.graca@hybris.com>
2015-04-30 14:23:56 +0200

    Added LambdaLabs solutions

--- src/main/java/com/xebia/java8_1/lambdas/LambdaLabs.java
+++ src/main/java/com/xebia/java8_1/lambdas/LambdaLabs.java
@@ -11,38 +11,45 @@ public class LambdaLabs
 	public static Persons filterUsingAnonymousInnerClass(final Persons persons)
 	{
 		//TODO: 1.1 Filter adults using a an anonymous inner class as input of the Persons.filter method.
-		return persons;
+		return persons.filter(new Persons.PersonFilter()
+		{
+			@Override
+			public boolean accept(final Person person)
+			{
+				return person.isAdult();
+			}
+		});
 	}
 
 	public static Persons filterWithLambda(final Persons persons)
 	{
 		//TODO: 1.2 Filter adults using a Lambda Expression as input of the Persons.filter method.
-		return persons;
+		return persons.filter(p -> p.isAdult());
 	}
 
 	public static Persons filterWithMethodReference(final Persons persons)
 	{
 		//TODO: 1.3 Filter adults using a Method Reference as input of the Persons.filter method.
-		return persons;
+		return persons.filter(Person::isAdult);
 	}
 
 	public static Persons filterMaleAdultsWithStaticMethodReference(final Persons persons)
 	{
 		//TODO: 1.4 Filter male adults using a static method reference as input of the Persons.filter method.
 		// Hint: use Persons.PersonSelector class
-		return persons;
+		return persons.filter(Persons.PersonSelector::maleAdults);
 	}
 
 	public static Persons sortPersonsWithLambda(final Persons persons)
 	{
 		//TODO: 1.5 Sort the persons by name using a Lambda Expression with two parameters as input of the Persons.sort method.
-		return persons;
+		return persons.sort((p1, p2) -> p1.getName().compareTo(p2.getName()));
 	}
 
 	public static Persons sortWithComparing(final Persons persons)
 	{
 		//TODO: 1.6 Sort the persons by name by creating a Comparator with the Comparator.comparing method.
 		// Use a Method Reference to define the sorting key (getName) as argument of the comparing method.
-		return persons;
+		return persons.sort(Comparator.comparing(Person::getName));
 	}
 }
